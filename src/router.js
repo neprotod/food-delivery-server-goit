174fde3fad4ee563b4d7a14e6bc68e7b1c560d7b
Alexander Podlesnyi <gwebdzen@gmail.com>
2019-11-02 03:20:39 +0200

    Add router distributor

---
+++ src/router.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+
+const connection = (req,res,routName,params)=>{
+    try{
+        const router = require('./routers/'+routName);
+
+        return router(req,res,...params);
+    }catch(e){
+        res.writeHead(404, {"Content-Type": "text/html"});
+        return `There isn't router "${routName}" in application`;
+    }
+
+    
+    
+}
+
+module.exports = {connection}
