b3655fbac1277034c1fe1e557fbb771ceec65a18
Alexander Podlesnyi <gwebdzen@gmail.com>
2019-11-29 00:48:52 +0200

    Add check validation for all

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -16,5 +16,19 @@ module.exports = {
         }
 
         return allMessage;
+    },
+
+    allValidation(req, res, shema){
+        const validation = shema.validate(req.body, {abortEarly: false});
+        // Get all errors validation
+        const errorMessages = this.getMessages(validation);
+        if(errorMessages){
+            console.error(validation.error);
+            res.status(400).json({errors: errorMessages});
+            return false;
+        }
+
+        req.body = validation.value;
+        return true;
     }
 }
